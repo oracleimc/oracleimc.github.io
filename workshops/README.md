548229a07e5df71225673e7c38eaadd86d97b6e6
Catalin Pohrib <46741243+cpohrib@users.noreply.github.com>
2019-08-13 11:29:08 +0300

    Added first draft of workshops request app
    First draft of workshops request app

---
+++ workshops/README.md
@@ -0,0 +1,3 @@
+# angular-itr2l3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-itr2l3)
---
+++ workshops/src/app/products.ts
@@ -0,0 +1,51 @@
+export const products = [
+  {
+    name: 'IMC0001',
+    duration: 2,
+    shortdescription: 'Oracle Database Upgrade Workshop',
+    fulldescription:'Upgrading to latest of Oracle Database provides the latest in efficient, reliable, secure data management for mission-critical on-line transaction processing applications, query-intensive data warehouses, and cloud applications. The right planning, preparation, and upgrade steps will make the upgrade process simpler, faster, and more predictable from start to finish. Oracles database utilities include Data Pump, SQL*Loader, Transportable Tablespaces, External Tables, and the original exp/imp utilities. These are all ways of moving data into or between Oracle databases, including cross-version and cross-platform migration.'
+  },
+  {
+    name: 'IMC0002',
+    duration: 2,
+    shortdescription: 'Architectural Principles and Best Practices for Adopting Oracle Cloud',
+    fulldescription:"Principles and Best Practices for Cloud Adoption, General considerations, Oracle Cloud Overview & Strategy, deciding on the cloud approach, describing the simplified workflow for getting on board the Cloud,overview of roles in the journey to the Cloud. This training will equip solution architects with the skills required to embark on a journey of assessing their current on-premises environment, defining the IT and non-IT goals for migrating to the cloud, understanding the operational targets, deciding the cloud topology, planning the migration tasks, and conducting the migration process. This involves answering a series of specific questions that will progressively help students narrow down the range of cloud deployment options and develop a migration strategy that's built on an optimal blend of Oracle Cloud service offerings."
+  },
+  {
+    name: 'IMC0005',
+    duration: 1.5,
+    shortdescription: 'Oracle Security and Compliance for Oracle Database',
+    fulldescription:'Security and Compliance with Oracle Database 12c. The Database security workshop showcases the following aspects: Transparent Data Encryption- Protection against database bypass threats....'
+  },
+  {
+    name: 'IMC0006',
+    duration: 3,
+    shortdescription: 'Revolutionizing Development with Oracle Cloud PaaS Services / out of date (could)',
+    fulldescription:"Simplify Applications Development Lifecycle, Team Collaboration and Management and Deployments with the Oracle Cloud Services. Development teams are looking for agile and scalable ways to deliver applications, while business is looking for quicker turn around time on application development; hence the need for Continuous Delivery is introduced. This workshop will give a hands on depth to Simplify Applications Development Lifecycle, Team Collaboration and Management and Deployments with the Oracle Cloud Services. Using Oracle Developer Cloud Service, teams can automate testing and Continuous Integration to allow software to be developed to a high standard, and using Oracle Java Cloud Service and Database Cloud Service, easily packaged and deployed to test environments, resulting in the ability to rapidly, reliably and repeatedly push out enhancements and bug fixes to customers at low risk and with minimal manual overhead."
+
+  },
+  {
+    name: 'IMC0009.A',
+    duration: 1,
+    shortdescription: 'Oracle Digital Assistant and Chatbots',
+    fulldescription:'Oracle Digital Assistant is a platform for developing apps for natural conversational interfaces such as Facebook Messenger and Amazon Alexa. The platform makes it easy to build sophisticated digital assistants or simple Chatbots that can connect and extend multiple backend systems, like Oracle ERP, HCM and CX, or any third-party apps - simultaneously, in a secure, scalable manner.'
+  },
+{
+    name: 'IMC0009.B',
+    duration: 2,
+    description: 'Oracle Digital Assistant and Chatbots'
+  },
+  {
+    name: 'IMC0009.C',
+    duration: 3,
+    shortdescription: 'Oracle Digital Assistant and Chatbots',
+    fulldescription:'adadsasdadadadasssssssssssssssss'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
