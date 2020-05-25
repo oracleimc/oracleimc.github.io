ec32c22b130fb2cbf0b765a188bad355895098de
Thanos Terentes Printzios <50945760+aterentes@users.noreply.github.com>
2020-05-25 09:46:24 +0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -3,18 +3,18 @@ jQuery(function ($) {
 	{
     entryTemplate:'<li><a href="{url}">[{author}@{date}] {title}</a><br/></li>, limit: 5
   })
-})
+}),
 
 jQuery(function ($) {
   $("#rss-customers").rss("https://www.oracle.com/search/customers/feed",  
 	{
     entryTemplate:'<li><a href="{url}">[{author}@{date}] {title}</a><br/></li>, limit: 5
   })
-})
+}),
 
 jQuery(function ($) {
   $("#rss-blogs").rss("http://feeds.feedburner.com/oracle/github", 
 	{
     entryTemplate:'<li><a href="{url}">[{author}@{date}] {title}</a><br/></li>, limit: 5
   })
-})
+});
